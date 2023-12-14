905b6cf57707b7be44f859dd857ce28d16a40237
L2G FiVi <uweschuetz@gmx.net>
2023-12-14 11:11:27 +0100

    Initial commit

---
+++ dist/game/data/scripts/quests/QuestMasterHandler.java
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the L2J Mobius project.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package quests;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import quests.Q10001_NewPath.Q10001_NewPath;
+import quests.Q10002_TrainingBeginsNow.Q10002_TrainingBeginsNow;
+
+/**
+ * @author NosBit
+ */
+public class QuestMasterHandler
+{
+	private static final Logger LOGGER = Logger.getLogger(QuestMasterHandler.class.getName());
+	
+	private static final Class<?>[] QUESTS =
+	{
+		Q10001_NewPath.class,
+		Q10002_TrainingBeginsNow.class,
+	};
+	
+	public static void main(String[] args)
+	{
+		for (Class<?> quest : QUESTS)
+		{
+			try
+			{
+				quest.getDeclaredConstructor().newInstance();
+			}
+			catch (Exception e)
+			{
+				LOGGER.log(Level.SEVERE, QuestMasterHandler.class.getSimpleName() + ": Failed loading " + quest.getSimpleName() + ":", e);
+			}
+		}
+	}
+}
